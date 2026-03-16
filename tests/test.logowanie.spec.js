3b80fff29cabe6871fd69ab9e5d6da7b0088e17d
przemek <testeroprogramowaniakontakt@gmail.com>
2026-03-16 19:21:57 +0100

    login +4

--- tests/test.logowanie.spec.js
+++ tests/test.logowanie.spec.js
@@ -10,6 +10,12 @@ test('has title', async ({ page }) => {
   await page.getByTestId('login-username').fill(process.env.ADMIN_LOGIN);
   await page.getByTestId('login-password').fill(process.env.ADMIN_PASSWORD);
   await page.getByTestId('login-button').click();
-  await expect(page.getByTestId('welcome-msg')).toContainText(`Witaj: ${process.env.USER}`);
+  await expect(page.getByTestId('welcome-msg')).toContainText(`Witaj: admin`);
 
-});
+});await page.getByTestId('login-username').click();
+await page.getByTestId('login-username').fill('admin');await expect(page.getByTestId('welcome-msg')).toMatchAriaSnapshot(`- text: "Witaj: admin"`);
+await page.getByTestId('login-username').press('Tab');
+await page.getByTestId('login-password').fill('admin');
+await page.getByTestId('login-button').click();
+await page.getByTestId('welcome-msg').click();
+await expect(page.getByTestId('welcome-msg')).toMatchAriaSnapshot(`- text: "Witaj: admin"`);
