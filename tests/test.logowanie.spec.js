085867b1066d6b908f242b533cfb80b68d4ae5a1
przemek <testeroprogramowaniakontakt@gmail.com>
2026-03-16 19:17:01 +0100

    login + 4

--- tests/test.logowanie.spec.js
+++ tests/test.logowanie.spec.js
@@ -7,8 +7,8 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
 
   await page.goto('/');
-  await page.getByTestId('login-username').fill(process.env.USER);
-  await page.getByTestId('login-password').fill(process.env.PASSWORD);
+  await page.getByTestId('login-username').fill(process.env.ADMIN_LOGIN);
+  await page.getByTestId('login-password').fill(process.env.ADMIN_PASSWORD);
   await page.getByTestId('login-button').click();
   await expect(page.getByTestId('welcome-msg')).toContainText(`Witaj: ${process.env.USER}`);
 
